73e3c38b0a35565a060bc33643e208b4d2bc9ccb
Zero_4793 <xxcrimsontardisxx@gmail.com>
2025-01-23 17:19:46 +1100

    +camera centering and averaging by mass !some glitchy/jumpy movements

--- apps/gravity.py
+++ apps/gravity.py
@@ -1,12 +1,13 @@
 from util.ball import Ball
 import random
+import pygame
 
 class Gravity:
 	def __init__(self, screen, _):
 		self.screen = screen
 
 		self.balls = []
-		for i in range(128):
+		for i in range(6):
 			self.balls.append(Ball(screen,
 						  pos=(random.randint(0,screen.get_width()), random.randint(0,screen.get_height())),
 						  vel=(random.uniform(-5,5), random.uniform(-5,5)),
@@ -14,22 +15,31 @@ class Gravity:
 						  repel=0,
 						  spaceJelly=0,
 						  collide=True,
-						  merging=True,
+						  merging=False,
 						  elasticity=.99,
-						  radius = random.randint(10,10),
+						  radius = random.randint(5,50),
 						  ))
 
 
 	def process(self, keyheld, keypressed):
+		camera = pygame.math.Vector2(0,0)
+		mass = 0
 		for ball in self.balls:
+			camera += ball.pos*ball.mass
+			mass += ball.mass
 			for other in self.balls:
 				if ball == other: continue
 				ball.force(other)
 		for ball in self.balls:
 			ball.process()
-			ball.wallCollide()
+			# ball.wallCollide()
 			if not ball.exist:
 				self.balls.remove(ball)
+		# camera/= len(self.balls)
+		camera/= mass
+		camera-= pygame.math.Vector2(self.screen.get_width()/2, self.screen.get_height()/2)
+		for ball in self.balls:
+			ball.pos -= camera		
 
 
 	def display(self):
